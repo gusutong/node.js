e7bb3924755d79681b7a74ad6ee833ef2683c514
gusutong <562656798@qq.com>
2018-09-18 15:25:10 +0800

    update

---
+++ nodejs-demo/03-console/01-format.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const user = {
+  name:"gusutong",
+  age:20,
+  qq:"562656798"
+};
+
+const log = console.log;
+
+//method1
+
+log("name: %s",user.name);
+log("age: %d",user.age);
+log("user: %j",user);
+
+//method2
+
+log("qq: " + user.qq);
+
+//method3
+
+log(`qq: ${user.qq}`);
