b62ca0a8fc34ed8ce23eef1e05e331694671e745
gusutong <562656798@qq.com>
2018-10-16 18:38:16 +0800

    exercise

--- nodejs-demo/10-file-system/06-my-rm.js
+++ nodejs-demo/10-file-system/06-my-rm.js
@@ -9,4 +9,26 @@ if(typeof(src) === 'undefined'){
   process.exit(1);
 }
 
-if(fs)
+if(!fs.existsSync(src)) {
+  console.error('%s not exist!', src);
+  process.exit(2);
+}
+
+if(fs.statSync(src).isFile()) fs.unlinkSync(src);
+
+if(fs.statSync(src).isDirectory()) deleteDir(src);
+
+function deleteDir(folder);
+
+for(var i=0; i<files.length;i++){
+  var file = join(folder,files[i]);
+
+    if(fs.statSync(file).isFile()) {
+      fs.unlinkSync(file);
+      continue;
+    } 
+
+  if(fs.statSync(file).isDirectory()) deleteDir(file);
+  }
+  fs.rmdirSync(folder);
+}
